40376e15ea6ed5ae0e2a41debc5b55a107e9941b
Melvin Folkers <melvin@yellowstacks.nl>
2020-01-08 22:52:36 +0100

    kleine update

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-from .scripts.functions import  set_logging, RunParameters
-from .scripts.pull_data import import_all
-from .scripts.upload import upload_all
-from datetime import datetime
-import atexit
-from functions import offices_in_scope
+from scripts.functions import  set_logging, RunParameters
+from scripts.pull_data import import_all
+from scripts.upload import upload_all
 
-from exitaction import exitfunction
 
-from functions import update_runstate
 def mainscript(run_params, jaar, refresh, upload):
     start = set_logging(run_params)
 
@@ -18,22 +13,10 @@ def mainscript(run_params, jaar, refresh, upload):
         upload_all(jaar, run_params, start)
 
 
-def mainflask(run_params):
-    start = datetime.now()
+if __name__ == "__main__":
 
-    update_runstate(run_params, runstate=4)
+    jaren = ['2020']
 
-    atexit.register(exitfunction, run_params=run_params)
-
-    if run_params.refresh.data:
-        #print('hierzo ->', offices_in_scope(run_params))
-        import_all(run_params, jaar=run_params.jaar.data, offices = offices_in_scope(run_params))
-
-    if run_params.upload.data:
-        upload_all(run_params.jaar.data, run_params, start)
-
-    update_runstate(run_params, runstate=2)
-
-
-
-
+    for jaar in jaren:
+        run_params = RunParameters(jaar)
+        mainscript(run_params, jaar, refresh = True, upload = True)

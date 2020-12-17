fd7d38f484a1ed3f6c0ddebf4a91c460d7b7be96
Melvin Folkers <melvin@yellowstacks.nl>
2020-12-17 14:17:07 +0100

    fix for relative file paths

--- twinfield/modules.py
+++ twinfield/modules.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 import pandas as pd
 import requests
+import os
 from . import templates
 from twinfield.functions import parse_session_response, parse_response, get_metadata
 
@@ -20,7 +21,9 @@ def read_offices(param) -> pd.DataFrame:
     """
 
     url = f"https://{param.cluster}.twinfield.com/webservices/processxml.asmx?wsdl"
-    body = templates.import_xml("xml_templates/template_list_offices.xml").format(param.session_id)
+    body = templates.import_xml(os.path.join("xml_templates", "template_list_offices.xml")).format(
+        param.session_id
+    )
     response = requests.post(url=url, headers=param.header, data=body)
 
     data = parse_session_response(response, param)
@@ -53,7 +56,7 @@ def read_module(param, periode, module, jaar=None) -> pd.DataFrame:
 
     url = f"https://{param.cluster}.twinfield.com/webservices/processxml.asmx?wsdl"
     if module in ["100", "200"]:
-        body = templates.import_xml(f"xml_templates/template_{module}.xml").format(
+        body = templates.import_xml(os.path.join("xml_templates", f"template_{module}.xml")).format(
             param.session_id, periode["from"], periode["to"]
         )
     elif module in ["030_1", "040_1"]:
@@ -61,14 +64,14 @@ def read_module(param, periode, module, jaar=None) -> pd.DataFrame:
             raise ValueError(
                 "Let op: je runt nu consolidatie / transacties, maar jaar is niet opgegeven."
             )
-        body = templates.import_xml(f"xml_templates/template_{module}.xml").format(
-            param.session_id, jaar, periode["from"], jaar, periode["to"]
-        )
+        body = templates.import_xml(
+            os.path.join("xml_templates", f"template_{module}.xml")
+        ).format(param.session_id, jaar, periode["from"], jaar, periode["to"])
     elif module.startswith("dimensions"):
         dim_type = module.split("_")[1].upper()
-        body = templates.import_xml("xml_templates/template_dimensions.xml").format(
-            param.session_id, dim_type
-        )
+        body = templates.import_xml(
+            os.path.join("xml_templates", "template_dimensions.xml")
+        ).format(param.session_id, dim_type)
     else:
         logging.info("Let op module is nog niet ontwikkeld")
     response = requests.post(url=url, headers=param.header, data=body)

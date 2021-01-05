6849fd6b6b7b8ab178b3b06c9647649392a42b03
Erfan Nariman <erfannariman@gmail.com>
2021-01-05 18:02:23 +0100

    release 1.0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as fp:
 
 setup(
     name="twinfield",
-    version="1.0.7",
+    version="1.0.8",
     author="Melvin Folkers, Erfan Nariman",
     author_email="melvin@zypp.io, erfan@zypp.io",
     description="Read and insert data using the Twinfield API.",

4918ec66e0b6ddb272704fc3de019279539742bf
Melvin Folkers <melvin@zypp.io>
2022-08-08 21:21:59 +0200

    added XML for deleted transactions

--- twinfield/messages.py
+++ twinfield/messages.py
@@ -80,3 +80,20 @@ LIST_OFFICES_XML = """<?xml version="1.0" encoding="utf-16"?>
         </twin:ProcessXmlString>
     </soapenv:Body>
 </soapenv:Envelope>"""
+
+DELETED_TRANSACTIONS_XML = """<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
+    <s:Header>
+        <h:Authentication xmlns:h="http://www.twinfield.com/" xmlns:i="http://www.w3.org/2001/XMLSchema-instance">
+            <AccessToken xmlns="http://schemas.datacontract.org/2004/07/Twinfield.WebServices.Shared">{}</AccessToken>
+            <CompanyCode xmlns="http://schemas.datacontract.org/2004/07/Twinfield.WebServices.Shared">{}</CompanyCode>
+        </h:Authentication>
+    </s:Header>
+    <s:Body>
+        <Query i:type="b:GetDeletedTransactions" xmlns="http://www.twinfield.com/" xmlns:a="http://schemas.datacontract.org/2004/07/Twinfield.WebServices" xmlns:i="http://www.w3.org/2001/XMLSchema-instance" xmlns:b="http://schemas.datacontract.org/2004/07/Twinfield.WebServices.DeletedTransactionsService">
+            <b:CompanyCode>{}</b:CompanyCode>
+            <b:DateFrom>{}</b:DateFrom>
+            <b:DateTo>{}</b:DateTo>
+        </Query>
+    </s:Body>
+</s:Envelope>
+"""

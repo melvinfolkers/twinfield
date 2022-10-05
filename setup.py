aee41c8736041928e2b516cf9f51586b45799467
Melvin Folkers <melvin@zypp.io>
2022-10-06 01:52:28 +0200

    updated version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as fp:
 
 setup(
     name="twinfield",
-    version="2.1.0-rc.3",
+    version="2.1.0-rc.4",
     author="Melvin Folkers, Erfan Nariman",
     author_email="melvin@zypp.io, erfan@zypp.io",
     description="Read and insert data using the Twinfield API.",

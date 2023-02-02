95e6fd62699aa328c588035dc705af672dc670ea
Tim van der Heijden <81814590+TimvdHeijden@users.noreply.github.com>
2023-02-02 12:28:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as fp:
 
 setup(
     name="twinfield",
-    version="2.1.0",
+    version="2.1.1",
     author="Zypp",
     author_email="hello@zypp.io",
     description="Read and insert data using the Twinfield API.",
